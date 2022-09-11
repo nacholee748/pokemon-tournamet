e689a0bd709dd726dd52d5d91abea48827b713e2
Jorge Ignacio Morales <jorge.morales@multienlace.com.co>
2022-09-11 01:18:09 -0500

    update changes

---
+++ src/lib/battle.py
@@ -0,0 +1,150 @@
+import random
+from dataclasses import dataclass
+from typing import Dict, List, Tuple
+
+from lib.pokemon import Pokemon
+
+@dataclass
+class BattleRound:
+    attacker: str
+    defendant: str
+    damage: float
+    ability: str
+
+@dataclass
+class BattleSummary:
+    winner: Pokemon
+    defeated: Pokemon
+    rounds: List[BattleRound]
+
+
+def simulate_battle(p1: Pokemon, p2: Pokemon, random_seed: int = 0) -> BattleSummary:
+    """This function simulates a single battle between two Pokémons.
+
+    A battle consists of several rounds, at each round there is only one Pokémon
+    doing the attack, while the other is defending. The role at each turn
+    is defined randomly and each participant has a random chance of being
+    selected as the attacker, weigthed by the amount abilities of the
+    participant over the total number of abilities of both participants. For
+    example, if `p1` has 2 abilities, while `p2` has only one, then `p1` is
+    expected to be selected as the attacker 66.66% of the time.
+
+    Since a battle is a random process, we will repeat each battle a thousand
+    times and pick the winner as the most common winner in all battles, as well
+    as picking the least number of rounds in which the winner won.
+
+    Each battle comes to an end whenever the health points of any of the
+    Pokémons reaches 0 or there are 100 rounds. At each turn of the battle,
+    the health points of the attacker remain the same, while the ones for the
+    defendant are computed as:
+
+        damage = attacker.attack * random.betavariate(1, 5)
+        HP_t+1 = HP_t - damage (Health points of the defendant at time t)
+
+
+    This function is provided a random_seed value, so that the battle always
+    yields the same results.
+    """
+    if p1.name == p2.name:
+        return BattleSummary(winner=p1,defeated=p2,rounds=[])
+
+    random.seed(random_seed)
+    winner_count = {p1.name:0,p2.name:0}
+    winner_rounds = {p1.name:[],p2.name:[]}
+
+    for _ in range(1000):
+        result = one_battle(p1,p2)
+        winner_count[result.winner.name] += 1
+
+        if winner_rounds[result.winner.name]:
+            if len(result.rounds) < len(winner_rounds[result.winner.name]):
+                winner_rounds[result.winner.name] = result.rounds
+        else:
+            winner_rounds[result.winner.name] = result.rounds
+
+    winner_name = sorted(winner_count.items(), key=lambda x: x[1])[0][0]
+    if winner_name == p1.name:
+        defeated =p2
+        winner = p1
+    else:
+        defeated =p1
+        winner = p2
+
+    summary = BattleSummary(winner=winner,defeated=defeated,rounds=winner_rounds)
+    
+    return summary
+
+def one_battle(p1: Pokemon,p2: Pokemon) -> BattleSummary:
+    rounds = []
+    probability_of_p1_begin_attacker = len(p1.abilities)/(len(p1.abilities)+len(p2.abilities))
+    health_points = {p1.name: p1.health_points,p2.name: p2.health_points}
+
+    while len(rounds)<=100 and (health_points[p1.name] > 0 and health_points[p2.name] > 0):
+        if random.random() <= probability_of_p1_begin_attacker:
+            attacker = p1
+            defendant = p2
+        else:
+            attacker = p2
+            defendant = p1
+
+        damage = attacker.attack * random.betavariate(1,5)
+        health_points[defendant.name] -= damage
+
+        rounds.append(
+            BattleRound(
+                attacker=attacker.name,
+                defendant=defendant.name,
+                damage=damage,
+                ability=random.choice(attacker.abilities)
+            )
+        )
+
+        defeated_name=sorted(health_points.items(), key = lambda x: x[1])[0][0]
+        if defeated_name == p1.name:
+            defeated = p1
+            winner = p2
+        else:
+            defeated = p2
+            winner = p1
+        
+        summary = BattleSummary(winner=winner, defeated=defeated, rounds=rounds)
+
+        return summary
+
+
+
+# #########################################
+# simulate_battle(Pokemon(name = "pok1",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok2",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 3)
+
+# one_battle(Pokemon(name = "pok1",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok2",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3))
---
+++ src/lib/tournament.py
@@ -0,0 +1,125 @@
+import random
+from typing import List, Sequence, Tuple
+from unittest import result
+
+from joblib import Parallel, delayed # type: ignore
+
+from lib.battle import BattleSummary, Pokemon, simulate_battle
+
+
+class Tournament:
+    """A class to simulate a tournament between pokemons.
+
+    Args:
+        participants: the pokemons that will participate in the tournament.
+        num_arenas: the available arenas to run battles simoultaneously.
+
+    """
+
+    def __init__(
+        self, participants: Sequence[Pokemon], num_arenas: int, random_seed: int = 0
+    ) -> None:
+        self._paticipants = participants
+        self._num_arenas = num_arenas
+        self._next_matches = _create_matches(participants)
+        random.seed(random_seed)
+
+    def run_stage(self) -> List[BattleSummary]:
+        """Runs all the battles defined in the current set of matches.
+
+        After running this method, the property `current_matches` gets
+        updated.
+
+        This method returns the summary of each battle run during the stage.
+        """
+
+        results  = Parallel(n_jobs=self._num_arenas)(
+            delayed(simulate_battle) (p1,p2)
+            for p1, p2 in self.next_matches
+        )
+
+        next_participants = [s.winner for s in results]
+        next_matches = _create_matches(next_participants)
+
+        if len(next_matches) == 1 and next_matches[0][0].name == next_matches[0][1].name:
+            next_matches = []
+        
+        self._next_matches = next_matches
+
+        return results
+
+    @property
+    def next_matches(self) -> List[Tuple[Pokemon, Pokemon]]:
+        return self._next_matches
+
+
+def _create_matches(participants: Sequence[Pokemon])-> List[Tuple[Pokemon, Pokemon]]:
+    """Takes a sequence of Pokémons and matches them randomly.
+
+    If the sequence is not even then one participant will be matched with
+    itself. No matter the input, this should happen at most once. If there
+
+    `random_seed` is used for reproducible results.
+    """
+    participants = random.sample(participants,len(participants))
+    participants_next_match = []
+
+    if len(participants)%2:
+        last_poke = participants.pop()
+        participants_next_match.append((last_poke,last_poke))
+    
+    for i in range(0,len(participants),2):
+        participants_next_match.append((participants[i],participants[i+1]))
+
+    return participants_next_match
+
+
+##########################3
+# _create_matches([Pokemon(name = "pok1",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok2",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok3",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok4",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok5",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3),
+#                 Pokemon(name = "pok2",
+#                 generation =  "gen",
+#                 type = "c",
+#                 abilities = ("1","2"),
+#                 health_points = 1.2,
+#                 attack = 1,
+#                 defense = 2,
+#                 speed =  3)])
